ccce82f89bbc0a10e6726770862119c2d6997ae8
Nihal <62196755+nihal4@users.noreply.github.com>
2025-01-18 17:18:30 +0600

    Update CWPO.py

--- optimizers/CWPO.py
+++ optimizers/CWPO.py
@@ -31,7 +31,7 @@ def dynamic_hazard(t, omega, alpha, beta, p, cat_pos, hazard_pos):
     return alpha * distance + periodic_fluctuation
 
 
-def CWPO(objf, lb, ub, dim, SearchAgents_no, Max_iter, alpha=1.0, beta=0.5, omega_freq=0.1, sf=5.0, levy_lambda=1.5, p=-1):
+def CWPO(objf, lb, ub, dim, SearchAgents_no, Max_iter, alpha=5.0, beta=0.5, omega_freq=0.1, sf=5.0, levy_lambda=1.5, p=-1):
     """
     Cat Water Phobia Optimizer (CWPO)
     objf: Objective function
